e69c55a15e3d8c09b4a1b5ff1f75eabbd05da458
Jeston Lewis <jeston.lewis@protonmail.com>
2023-04-08 22:55:42 -0600

    basic test function for retrieving from a dynamodb

--- IaC/index.py
+++ IaC/index.py
@@ -1,5 +1,17 @@
 import boto3
 
 dynamo = boto3.client('dynamodb')
+dynamor = boto3.resource('dynamodb')
+table = dynamor.Table('breadtest') # type: ignore
 
 # request a sum of items and count backwards from the the total for the most recent item
+def lambda_handler(event, context):
+    posts = table.item_count('breadtest')
+    print(posts)
+
+    response = dynamo.get_item(
+        Key = {'post_num':{'N':'0'}},
+        TableName = 'breadtest'
+    )
+
+    return response
