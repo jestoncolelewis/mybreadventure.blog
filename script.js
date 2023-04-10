dac5a3bc2c509693cf1d34b2099674cfe6be35a4
Jeston Lewis <jeston.lewis@protonmail.com>
2023-04-10 07:16:20 -0600

    made it so multiple posts can be pulled from the database and displayed

--- script.js
+++ script.js
@@ -1,10 +1,4 @@
 const posts = document.querySelector('.posts')
-const post = document.createElement('div')
-post.setAttribute('class', 'post')
-const title = document.createElement('h2')
-const excerpt = document.createElement('p')
-const image = document.createElement('img')
-posts.append(title)
 
 function get_title(content) {
     return content.Item.post_title.S;
@@ -18,27 +12,36 @@ function get_image(content) {
     return content.Item.post_feature.S;
 }
 
-let url = 'https://t2646lcfvh.execute-api.us-west-2.amazonaws.com/retrieveTest';
+const url = 'https://t2646lcfvh.execute-api.us-west-2.amazonaws.com/retrieveTest';
 fetch(url)
-.then((response) => response.text())
-.then((data) => {
-    let items = data.split('", "');
-    let item = items[0]
-    console.log(item);
-    let response = JSON.parse(item);
+    .then((response) => response.text())
+    .then((data) => {
+        const nosngl = data.replaceAll("'", '"');
+        const items = nosngl.split('^*^');
+        for (let i = 0; i < items.length; i++) {
+            const post = document.createElement('div')
+            post.setAttribute('class', 'post')
+            const title = document.createElement('h2')
+            const excerpt = document.createElement('p')
+            const image = document.createElement('img')
+            posts.append(title)
+            
+            console.log(items[i]);
+            const response = JSON.parse(items[i]);
 
-    // post
-    posts.append(post);
+            // post
+            posts.appendChild(post);
 
-    // image
-    image.src = get_image(response);
-    post.append(image);
+            // image
+            image.src = get_image(response);
+            post.append(image);
 
-    // title
-    title.textContent = get_title(response);
-    post.append(title);
+            // title
+            title.textContent = get_title(response);
+            post.append(title);
 
-    // excerpt
-    excerpt.textContent = get_excerpt(response);
-    post.append(excerpt);
-})
+            // excerpt
+            excerpt.textContent = get_excerpt(response);
+            post.append(excerpt);
+        }
+    })
