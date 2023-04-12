9724e542567fd7285a08293cac7962497484bc3d
Jeston Lewis <jeston.lewis@protonmail.com>
2023-04-12 11:54:00 -0500

    added functionality to find files in the parent directory

--- IaC/build.py
+++ IaC/build.py
@@ -13,14 +13,28 @@ path = path + "/" + file
 key = build_lambda_bucket(name, path, file)
 
 # s3 web variables
+web_path = os.path.abspath(os.path.join(path, os.pardir))
 
+files = [] # load all desired files and folders in web_path
 
-build_web_bucket(name, path, file)
+with os.scandir('..') as it:
+    for entry in it:
+        if not entry.name.startswith('.') and entry.is_file():
+            if not entry.name.endswith('.md'):
+                if not entry.name.endswith('.http'):
+                    files.append(entry.name)
+        if not entry.name.startswith('.') and entry.is_dir():
+            if not entry.name.startswith('IaC'):
+                files.append(entry.name)
+
+build_web_bucket(name, web_path, files)
 
 # dynamo variables
-keys = [{"AttributeName": "primary-key", "KeyType": "HASH"}]
+keys = [{"AttributeName": "post-num", "KeyType": "HASH"}]
 
-attributes = [{"AttributeName": "primary-key", "AttributeType": "N"}]
+attributes = [{"AttributeName": "post-num", "AttributeType": "N"}]
+
+build_dynamo(name, keys, attributes)
 
 # lambda variables
 lang = "python3.9"
@@ -30,3 +44,5 @@ iam = build_iam(name)
 code = [name, key]
 
 description = "function for retrieving and updating page views"
+
+build_lambda(name, lang, iam, code, description)
